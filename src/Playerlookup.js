96d98687ce8bbea712eddf1f68038dad1628c935
Jim <zhijianliang09@gmail.com>
2024-03-08 10:15:54 -0800

    refactor text input logic for UI rendering only

--- src/Playerlookup.js
+++ src/Playerlookup.js
@@ -1,18 +1,12 @@
-import React from "react";
-//import the api module 
-import ValorantAPI from 'unofficial-valorant-api';
-const valorantAPI = new ValorantAPI();
+import React from 'react';
 
-
-
-class Playerlookup extends React.Component {
+class PlayerLookup extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            input : ''
+            input: '',
         };
         this.handleTextInput = this.handleTextInput.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
     
     //gets called everytime user types in the input box 
@@ -20,31 +14,13 @@ class Playerlookup extends React.Component {
         this.setState({input: event.target.value});
     } 
 
-    async handleSubmit(event) {
-        //prevent reloading page when user presses enter
-        event.preventDefault();
-        const playerInfoArr = this.state.input.split('#');
-        console.log(playerInfoArr);
-        //check if information is in correct format: playerName#playerTag
-        if (playerInfoArr.length === 2) {
-            // Retrieve player data from the API
-            const playerData = await valorantAPI.getAccount({ name: playerInfoArr[0], tag: playerInfoArr[1] });
-            console.log(playerData);
-        } else {
-            console.log('Invalid input format');
-        }
-        
-    }
-
     render() {
         return(
             <div>
-                <form onSubmit={this.handleSubmit}>
                     <input type="input" value={this.state.input} onChange={this.handleTextInput} />
-                </form>
             </div>
         );
     }
 }
 
-export default Playerlookup;
+export default PlayerLookup;
